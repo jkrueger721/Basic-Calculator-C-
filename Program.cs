cc86ef6a1e9a034cd0061ad8e11e78ed250f81a5
John <johnathankrueger@me.com>
2018-12-17 20:09:55 -0500

    intitial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("x=");
+                string str = Console.ReadLine();
+                if (str == "exit")
+                    break;
+
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("y=");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                int sum y+x;
+                Console.WriteLine("Result = {0}",sum);
+
+
+            }
+            while (true);
+
+        }
+    }
+}
+    
